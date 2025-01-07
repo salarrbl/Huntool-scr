c99eedce05dd8d7a23de338b11709509f18a25dd
salarrbl <rblcrow0101@gmail.com>
2025-01-07 05:31:01 +0330

    extracting live subdomains

--- main.py
+++ main.py
@@ -38,12 +38,37 @@ def collect_subdomains():
             file.write(output)
     if (error2):
         print("Error:", error)
+
+    print("(*) assetfinder running :")
+    result3 = subprocess.run(['assetfinder', '-subs-only', target, ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    # Storing the result
+    output3 = result.stdout
+    error3 = result.stderr
+    if (output3):
+        with open('./subdomains/assetfinder_subs.txt', 'a') as file:
+            file.write(output)
+    if (error3):
+        print("Error:", error3)
+    
 # function for extract all subdomain with all file and give all_Subdomains.txt
 def all_Subdomains():
     # result_all = subprocess.run(['cat ./subdomains/*.txt | sort | uniq > all_Subdomains.txt'])
     files = glob.glob('./subdomains/*.txt')
     #my command
-    command = 'cat ' + ' '.join(files) + ' | sort | uniq > all_Subdomains.txt'
+    command = 'cat ' + ' '.join(files) + ' | sort | uniq > ./subdomains/all_Subdomains.txt'
     result = subprocess.run(['bash', '-c', command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-# collect_subdomains()
-all_Subdomains()
+
+
+
+
+
+
+def live_subs():
+    print("extracting Live Subdomains")
+    subprocess.run(['mkdir', 'live_subs'])
+    #httpx -silent -l all_Subdomains.txt -o live_subdomains.tx
+    subprocess.run(['httpx', '-silent', '-l', './subdomains/all_Subdomains.txt', '-o', './live_subs/live_subdomains.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+live_subs()
+
+
