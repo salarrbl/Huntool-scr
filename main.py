8320e92122f0d25c95305fa95ad2ae8d3ba237fd
salarrbl <rblcrow0101@gmail.com>
2025-01-07 03:57:32 +0330

    remove two file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import subprocess
 import sys
-
+import os
 if len(sys.argv) != 2:
     print("Usage: python script.py <target>")
     sys.exit(1)
@@ -9,15 +9,21 @@ if len(sys.argv) != 2:
 target = sys.argv[1]
 # function for subfinder
 def collect_subdomains():
-    print("subfinder running :")
     # Run Subfinder on the target 
+    direc_subs = './subdomains'
+    if os.path.exists(direc_subs):
+        print('')
+    else:
+        subprocess.run(['mkdir' , 'subdomains'])
+    subprocess.run(['bash', '-c', f'cd {'./subdomains'} && ls'])
+    print("subfinder running :")
     result = subprocess.run(['subfinder', '-d', target, '-silent'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
     # Storing the result
     output = result.stdout
     error = result.stderr
     if (output):
-        with open('Subfinder_subs.txt', 'a') as file:
+        with open('./subdomains/Subfinder_subs.txt', 'a') as file:
             file.write(output)
     if (error):
         print("Error:", error)
@@ -27,12 +33,12 @@ def collect_subdomains():
     output2 = result2.stdout
     error2 = result2.stderr
     if (output2):
-        with open('findomain_subs.txt', 'a') as file:
+        with open('./subdomains/findomain_subs.txt', 'a') as file:
             file.write(output)
     if (error2):
         print("Error:", error)
 collect_subdomains()
 # function for extract all subdomain with all file and give all_Subdomains.txt
 def all_Subdomains():
-    result_all = subprocess.run(['cat *.txt | sort | uniq > all_Subdomains.txt'])
-all_Subdomains()
+    result_all = subprocess.run(['cat ./subdomains/*.txt | sort | uniq > all_Subdomains.txt'])
+
