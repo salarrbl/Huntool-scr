283ab1beadade096aee457b7ed7cad6f5e751ccb
salarrbl <rblcrow0101@gmail.com>
2025-01-08 03:40:51 +0330

    work on script port scan

--- port_scan/script.sh
+++ port_scan/script.sh
@@ -6,35 +6,37 @@ if [ $# -eq 0 ]; then
 fi
 
 target=$1
+port_baner() {
+		
+	echo "Scanning ports 1-8000 on $target..."
 
-echo "Scanning ports 1-8000 on $target..."
+	for port in {1..8000}; do
+		timeout 1 bash -c "echo > /dev/tcp/$target/$port" 2>/dev/null && \
+		echo "Port $port is open"
+	done
 
-for port in {1..8000}; do
-    timeout 1 bash -c "echo > /dev/tcp/$target/$port" 2>/dev/null && \
-    echo "Port $port is open"
-done
+	echo "Scan complete."
+	DEFAULT_PORT="80"
 
-echo "Scan complete."
-DEFAULT_PORT="80"
+	ip=$1
+	if [[ "$ip" =~ ^[a-zA-Z]+$ ]]; then
+		ip=dig +short ${ip}
+	fi
 
-ip=$1
-if [[ "$ip" =~ ^[a-zA-Z]+$ ]]; then
-	ip=dig +short ${ip}
-fi
+	if [[ -z "${ip}" ]]; then
+	  echo "You must provide an IP address."
+	  exit 1
+	fi
 
-if [[ -z "${ip}" ]]; then
-  echo "You must provide an IP address."
-  exit 1
-fi
+	if [[ -z "${port}" ]]; then
+	  echo "You did not provide a specific port, defaulting to ${DEFAULT_PORT}"
+	  port="${DEFAULT_PORT}"
+	fi
 
-if [[ -z "${port}" ]]; then
-  echo "You did not provide a specific port, defaulting to ${DEFAULT_PORT}"
-  port="${DEFAULT_PORT}"
-fi
+	echo "Attempting to grab the Server header of ${ip}..."
 
-echo "Attempting to grab the Server header of ${ip}..."
+	result=$(curl -s --head "${ip}:${port}" | grep Server | awk -F':' '{print $2}') 
 
-result=$(curl -s --head "${ip}:${port}" | grep Server | awk -F':' '{print $2}') 
-
-echo "Server header for ${ip} on port ${port} is: ${result}"
-
+	echo "Server header for ${ip} on port ${port} is: ${result}"
+}
+port_baner  > open_ports.txt
